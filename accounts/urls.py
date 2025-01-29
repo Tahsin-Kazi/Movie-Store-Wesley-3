872f0ba6cf43d18a950799e1305f30315c46bd29
Tahsin-Kazi <tahsin.kazi35@gmail.com>
2025-01-29 16:04:13 -0500

    Added basic user authentication system

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from accounts.views import register, user_login, successful_login
+
+urlpatterns = [
+    path("register/", register, name="register"),
+    path("login/", user_login, name="login")
+]
---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from accounts.forms import RegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect("landing_page")  # Redirect to landing page
+    else:
+        form = RegisterForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)  # Log the user in
+                return successful_login(request)  # Redirect to a success page
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+def successful_login(request):
+    return render(request, 'accounts/temp_success.html')
