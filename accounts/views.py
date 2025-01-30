4ceb1221454587f93edcf939322aac07077a8de9
Ahad Sattar <ahadsattar007@gmail.com>
2025-01-29 23:49:56 -0500

    Added a bunch of formatting/visuals to all pages, started adding some filler data

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
+    template_data = {}
+    template_data = {'title': 'Register'}
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -13,9 +15,11 @@ def register(request):
             return redirect("login")  # Redirect to landing page
     else:
         form = RegisterForm()
-    return render(request, "accounts/register.html", {"form": form})
+    return render(request, "accounts/register.html", {"form": form, "template_data": template_data})
 
 def user_login(request):
+    template_data = {}
+    template_data = {'title': 'Login'}
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -27,7 +31,9 @@ def user_login(request):
                 return redirect(successful_login)  # Redirect to a success page
     else:
         form = AuthenticationForm()
-    return render(request, "accounts/login.html", {"form": form})
+    return render(request, "accounts/login.html", {"form": form, "template_data": template_data})
 
 def successful_login(request):
-    return render(request, 'accounts/temp_success.html')
+    template_data = {}
+    template_data = {'title': 'Success'}
+    return render(request, 'accounts/temp_success.html', {"template_data": template_data})
