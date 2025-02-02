76f1b2e24eba1efec6f5b36de6aa42581aaa4392
Jason-Bogtong <jason.bogtong@gmail.com>
2025-02-02 12:02:33 -0500

    Part of the movies model

--- movies/views.py
+++ movies/views.py
@@ -18,9 +18,10 @@ movies = [
         'description': 'A love story set against the backdrop of the sinking Titanic.',
     },
 ]
-def index(request):
+def index(request, id):
+    movie = movies[id - 1]
     template_data = {}
-    template_data['title'] = 'Movies'
+    template_data['title'] = movie['name']
     template_data['movies'] = movies
-    return render(request, 'movies/index.html',
+    return render(request, 'movies/show.html',
                   {'template_data': template_data})
