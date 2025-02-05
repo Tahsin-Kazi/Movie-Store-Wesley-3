040e97cbe9e4069b3f5aeb98416b9c77fe230b3d
Tahsin-Kazi <tahsin.kazi35@gmail.com>
2025-02-05 14:04:23 -0500

    Fixed edit_review

--- movies/urls.py
+++ movies/urls.py
@@ -2,16 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< Updated upstream
-    path('', views.index, name='movies.index'),
-    path('<int:id>', views.show, name='movies.show'),
-
-    #path for the reviews
-    path('reviews/edit/<int:review_id>/', views.edit_review, name='movies.edit_review'),
-
-=======
     path('movies/', views.index, name='movies.index'),
     path('movies/<int:id>/', views.show, name='movies.show'),
     path('reviews/edit/<int:review_id>/', views.edit_review, name='movies.edit_review'),
->>>>>>> Stashed changes
 ]
